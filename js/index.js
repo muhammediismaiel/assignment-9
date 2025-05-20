7dbc411a35e05e67305012490668b86ca4c68555
mohamed ismaiel <78222655+muhammediismaiel@users.noreply.github.com>
2025-05-21 02:28:37 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,8 +4,15 @@ const quotes = [
   "The moment you think of giving up, think of the reason why you held on so long – Natsu Dragneel",
   "You can’t sit around envying other people’s worlds. You have to go out and change your own – Sheryl Nome"
 ];
-
+let lastRandom = -1;
 function quoteChanger() {
+
   let random = Math.floor(Math.random() * quotes.length);
+
+  if (random === lastRandom) {
+    random = (random + 1) % quotes.length;
+  }
+
+  lastRandom = random;
   document.getElementById("quote").innerHTML = quotes[random];
 }
